7b2d02c582944ddc9785dda73d35e092bcca1ed3
BrunoCatais <bruno@seeworking.com>
2022-10-03 09:06:34 -0400

    [FEATURE] Adicionando inserção e alteração de artigos

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -3,7 +3,7 @@
     <v-card width="800" flat>
       <v-row>
         <v-col class="pa-0">
-          <span class="text--secondary text-h6">Article 1</span>
+          <span class="text--secondary text-h6">Article {{ project.currentArticle + 1 }}</span>
         </v-col>
       </v-row>
       <div class="px-4 my-8">
@@ -42,17 +42,23 @@
             <span class="text--secondary">File</span>
           </v-col>
         </v-row>
-        <v-row>
+        <v-row align="center">
+          <v-col cols="auto">
+            <v-file-input v-model="article.file" hide-details hide-input outlined></v-file-input>
+          </v-col>
           <v-col>
-            <v-file-input hide-details show-size single-line outlined></v-file-input>
+            {{ (article.file.name != null) ? article.file.name : article.file }}
           </v-col>
         </v-row>
       </div>
-      <v-row>
-        <v-col>
-          <v-btn width="150" color="green" class="white--text" @click="insertArticle()">submit</v-btn>
-        </v-col>
-      </v-row>
+      <v-card-actions class="pt-8">
+        <v-btn width="150" color="grey" outlined @click="redirectToPrevious()" :disabled="!canGoToPrevious()">previous
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn width="150" color="green" class="white--text" @click="insertArticle()" :disabled="!canGoNext()">next
+        </v-btn>
+      </v-card-actions>
+      {{shouldInsert()}}
     </v-card>
   </v-container>
 </template>
@@ -65,18 +71,56 @@ export default {
       article: {
         title: '',
         abstract: '',
-        keywords: ''
+        keywords: '',
+        file: {
+          name: ''
+        }
       }
     }
   },
   methods: {
     insertArticle() {
-      this.articles.push(this.article)
+      if (this.shouldInsert()) {
+        this.article.file = this.article.file.name
+        this.project.articles.push(this.article)
+      }
+      this.redirectToNext()
+    },
+    redirectToNext() {
+      this.project.currentArticle += 1
+      if (this.project.articles[this.project.currentArticle]) {
+        this.article = this.project.articles[this.project.currentArticle]
+      } else {
+        this.resetForm()
+      }
+    },
+    redirectToPrevious() {
+      this.project.currentArticle -= 1
+      this.article = this.project.articles[this.project.currentArticle]
+    },
+    canGoToPrevious() {
+      return this.project.currentArticle > 0
+    },
+    canGoNext() {
+      return this.article.title && this.article.file && this.article.keywords && this.article.abstract
+    },
+    resetForm() {
+      this.article = {
+        title: '',
+        abstract: '',
+        keywords: '',
+        file: {
+          name: ''
+        }
+      }
+    },
+    shouldInsert() {
+      return this.article !== this.project.articles[this.project.currentArticle]
     }
   },
   computed: {
-    articles() {
-      return this.$store.state.project.articles
+    project() {
+      return this.$store.state.project
     }
   }
 }
